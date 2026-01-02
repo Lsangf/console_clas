59a155e4e6c80fe5d18917232bbb3ecf7b39e419
Lsangf <z9333601@gmail.com>
2026-01-02 18:55:17 +0200

    добавил номера домов, и список всех цен

--- console_clas/Core_/GameCore.cs
+++ console_clas/Core_/GameCore.cs
@@ -7,18 +7,30 @@ namespace console_clas.Core_
     {
         public static void Main(List<HouseBase> houses, BuildingsCity[] buildings)
         {
+            short i = 0;
+
+            List<int> ListPrices = [];
+
             foreach (var house in houses)
             {
-                Console.WriteLine($"Площадь: {house.Square} Этажей: {house.Floors} Комант: {house.Rooms} Координаты: {house.CoordinatesHouse}");
+                Console.WriteLine($"Дом {i} | Площадь: {house.Square} Этажей: {house.Floors} Комант: {house.Rooms} Координаты: {house.CoordinatesHouse}");
 
                 house.PriceUpdate(buildings);
 
                 Console.WriteLine($"Цена: {house.Price} $");
                 Console.WriteLine();
+                ListPrices.Add(house.Price);
+                i++;
             }
             Console.WriteLine($"{buildings[0].Name}, {buildings[0].DistancePriceFactorFor1000m}, {buildings[0].CoordinatesBuildings}");
             Console.WriteLine($"{buildings[1].Name}, {buildings[1].DistancePriceFactorFor1000m}, {buildings[1].CoordinatesBuildings}");
             Console.WriteLine($"{buildings[2].Name}, {buildings[2].DistancePriceFactorFor1000m}, {buildings[2].CoordinatesBuildings}");
+
+            Console.Write($"All Prices: ");
+            foreach (var price in ListPrices)
+            {
+                Console.Write($"{price} $ | ");
+            }
         }
 
     }
